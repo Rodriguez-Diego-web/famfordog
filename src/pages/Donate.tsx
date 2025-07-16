f9dd7fb79af3f2dc5e87d137eb710cef4c158694
Diego Rodriguze <diego@rodriguez-digital.de>
2025-07-16 09:18:23 +0200

    Fix mobile iframe height and enable scrolling on donate page

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -45,13 +45,13 @@ const Donate = () => {
                 src="https://secure.fundraisingbox.com/app/payment?hash=9kl46fm02g6o9ev6&t=18642046f767d060416617baf23108df&fb_id=25408"
                 title="Fundraising Box Spendenformular"
                 width="100%"
-                height="2800"
-                className="md:h-[2800px]" 
+                height="3500"
+                className="h-[3500px] md:h-[2800px]" 
                 frameBorder="0"
                 allowTransparency={true}
                 allow="payment"
-                style={{ display: 'block', margin: '0 auto', overflow: 'hidden' }}
-                scrolling="no"
+                style={{ display: 'block', margin: '0 auto' }}
+                scrolling="yes"
               ></iframe>
             </div>
             
